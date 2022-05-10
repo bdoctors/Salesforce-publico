e4527279b1830bbfca329e88e0e8bbd1bcd47f93
Benjamin Doctors <101369245+bdoctors@users.noreply.github.com>
2022-05-10 13:15:00 -0300

    Update helloMessageInteractiveController.js
    add a bit of logging to handleClick2 function

--- Aura Components Basics/Handle Actions with Controllers/helloMessageInteractiveController.js
+++ Aura Components Basics/Handle Actions with Controllers/helloMessageInteractiveController.js
@@ -6,6 +6,7 @@
 	},
     	handleClick2: function(component, event, helper) {
         	let newMessage = event.getSource().get("v.label");
+		console.log("handleClick2: Message: " + newMessage);
         	component.set("v.message", newMessage);
     	},
     	handleClick3: function(component, event, helper) {
