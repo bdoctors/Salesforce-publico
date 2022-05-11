155e23570153f83c7c7c6704c18c6e717ac4f853
Benjamin Doctors <101369245+bdoctors@users.noreply.github.com>
2022-05-10 23:38:07 -0300

    Update campingListController.js
    If the form is valid, the JavaScript controller pushes the newItem onto the array of existing items, triggers the notification that the items value provider has changed, and resets the newItem value provider with a blank sObjectType of Camping_Item__c (for this challenge, place the code in your component's controller, not the helper)

--- Aura Components Basics/Input Data Using Forms/campingListController.js
+++ Aura Components Basics/Input Data Using Forms/campingListController.js
@@ -7,7 +7,22 @@
         }, true);
         // If we pass error checking, do some real work
         if(validCampingItem){ 
-         // Do your magic;
+             // Create the newItem
+            let newItem = component.get("v.newItem");
+            console.log("Create item: " + JSON.stringify(newItem));
+ 
+            let theItems = component.get("v.items");
+        
+            console.log("Antes: " + JSON.stringify(theItems));
+            theItems.push(newItem);
+            component.set("v.items", theItems);
+            console.log("Despues: " + JSON.stringify(theItems));
+            
+            component.set("v.newItem", {'sobjectType': 'Camping_Item__c',
+                        'Name': '',
+                        'Quantity__c': 0,
+                        'Price__c': 0,
+                        'Packed__c': false});
         }
         
     }
