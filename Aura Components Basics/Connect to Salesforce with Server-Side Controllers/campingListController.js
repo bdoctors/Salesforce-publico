6d4798173ec475677b7348fa813f799e9c7c5827
Benjamin Doctors <101369245+bdoctors@users.noreply.github.com>
2022-05-11 14:59:39 -0300

    Update campingListController.js
    Added doInit

--- Aura Components Basics/Connect to Salesforce with Server-Side Controllers/campingListController.js
+++ Aura Components Basics/Connect to Salesforce with Server-Side Controllers/campingListController.js
@@ -1,5 +1,23 @@
 ({
 
+    // Load data from Salesforce
+    doInit: function(component, event, helper) {
+        // Create the action
+        let action = component.get("c.getItems");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.items", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    },
+  
   clickCreateItem : function(component, event, helper) {
         let validCampingItem = component.find('campingItemForm').reduce(function (validSoFar, inputCmp) {
             // Displays error messages for invalid fields
