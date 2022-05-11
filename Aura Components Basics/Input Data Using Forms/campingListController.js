b3115bb8a56f2ead8294e542e6bc5f24ca641193
Benjamin Doctors <101369245+bdoctors@users.noreply.github.com>
2022-05-10 23:27:47 -0300

    Update campingListController.js
    Validate the form

--- Aura Components Basics/Input Data Using Forms/campingListController.js
+++ Aura Components Basics/Input Data Using Forms/campingListController.js
@@ -1,5 +1,14 @@
 ({
     clickCreateItem : function(component, event, helper) {
+        let validCampingItem = component.find('campingItemForm').reduce(function (validSoFar, inputCmp) {
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        // If we pass error checking, do some real work
+        if(validCampingItem){ 
+         // Do your magic;
+        }
         
     }
 })
