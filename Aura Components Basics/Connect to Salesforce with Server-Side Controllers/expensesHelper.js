c34e1510ca37e6f00507a2e68c4b4a181434c3e9
Benjamin Doctors <101369245+bdoctors@users.noreply.github.com>
2022-05-11 12:58:49 -0300

    Update expensesHelper.js
    Se elimina la manipulacion local y se usa el server controler.
    action
    callBack
    queue

--- Aura Components Basics/Connect to Salesforce with Server-Side Controllers/expensesHelper.js
+++ Aura Components Basics/Connect to Salesforce with Server-Side Controllers/expensesHelper.js
@@ -1,12 +1,21 @@
 ({
 	createExpense : function(component, expense) {
-        let theExpenses = component.get("v.expenses");
-        // Copy the expense to a new object
-        // THIS IS A DISGUSTING, TEMPORARY HACK
-        let newExpense = JSON.parse(JSON.stringify(expense));
-        console.log("Expenses before 'create': " + JSON.stringify(theExpenses));
-        theExpenses.push(newExpense);
-        component.set("v.expenses", theExpenses);
-        console.log("Expenses after 'create': " + JSON.stringify(theExpenses));
-	}
+        let action = component.get("c.saveExpense"); // Salvar con el metodo saveExpense del server controler
+        action.setParams({ "expense" : expense });   // Next we attach a data payload to the action. This is new. 
+		                                     // We need to send the data for the new expense up to the server. 
+		                                     // But look how easy it is! You just use action.setParams() and 
+		                                     // provide a JSON-style object with parameter name-parameter value pairs. 
+		                                     // The one trick, and it’s important, is that your parameter name must 
+		                                     // match the parameter name used in your Apex method declaration.
+            
+        action.setCallback(this, function(response){ // Next we set the callback for the request. Again, this is what will happen 
+	    let state = response.getState();         // when the server returns a response. If you compare this callback function 
+            if (state === "SUCCESS") {               // with our original createExpense helper function, it’s virtually identical (minus the disgusting hack).
+                let expenses = component.get("v.expenses");
+                expenses.push(responsegetReturnValue());
+                component.set("v.expenses", expenses);
+            }
+        });
+        $A.enqueueAction(action);
+	},
 })
