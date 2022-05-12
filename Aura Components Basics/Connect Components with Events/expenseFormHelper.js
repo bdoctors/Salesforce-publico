af6acc676e0e97aedb168f4b7caaba15a3e13bcd
Benjamin Doctors <101369245+bdoctors@users.noreply.github.com>
2022-05-12 17:49:30 -0300

    Update expenseFormHelper.js
    In the expenseForm helper, create the createExpense function.

--- Aura Components Basics/Connect Components with Events/expenseFormHelper.js
+++ Aura Components Basics/Connect Components with Events/expenseFormHelper.js
@@ -1,1 +1,7 @@
-
+({
+    createExpense: function(component, newExpense) {
+        let createEvent = component.getEvent("createExpense");
+        createEvent.setParams({ "expense": newExpense });
+        createEvent.fire();
+    },
+})
