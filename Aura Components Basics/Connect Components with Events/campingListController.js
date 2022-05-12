3189cd8727a7d4f54176127211ef0cf932433d83
Benjamin Doctors <101369245+bdoctors@users.noreply.github.com>
2022-05-12 20:06:34 -0300

    Update campingListController.js
    Replace the HTML form in the campingList component with a new campingListForm component that calls the clickCreateItem JavaScript controller action when clicked.
     6. Eliminar clickCreateItem de campingListController.js para llevarlo a campingListFormController.js

--- Aura Components Basics/Connect Components with Events/campingListController.js
+++ Aura Components Basics/Connect Components with Events/campingListController.js
@@ -17,21 +17,4 @@
         // Send action off to be executed
         $A.enqueueAction(action);
     },
-    
-    clickCreateItem : function(component, event, helper) {
-        let validCampingItem = component.find('campingItemForm').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-        // If we pass error checking, do some real work
-
-        if(validCampingItem){
-            // Create the newItem
-            let item = component.get("v.newItem");
-            console.log("Create item: " + JSON.stringify(item));
-            
-            helper.createItem(component, item);
-        }        
-    }
 })
