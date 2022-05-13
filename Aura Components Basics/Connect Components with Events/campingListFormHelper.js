723f4b24d38cedf47ccf82fae78cdd3c84f18744
Benjamin Doctors <101369245+bdoctors@users.noreply.github.com>
2022-05-12 23:04:09 -0300

    Update campingListFormHelper.js
    The campingListForm Helper JavaScript helper creates an addItem event with the item to be added, fires the event, then resets the newItem value provider with a blank sObjectType of type Camping_Item__c

--- Aura Components Basics/Connect Components with Events/campingListFormHelper.js
+++ Aura Components Basics/Connect Components with Events/campingListFormHelper.js
@@ -1,1 +1,13 @@
-
+({
+    createItem: function(component, event, helper) {
+        let item = component.get("v.item");
+        let addItemEvent = component.getEvent("addItem");
+        updateEvent.setParams({ "item": item });
+        updateEvent.fire();
+        component.set("v.newItem", {'sobjectType': 'Camping_Item__c',
+                        'Name': '',
+                        'Quantity__c': 0,
+                        'Price__c': 0,
+                        'Packed__c': false});
+    }
+})
